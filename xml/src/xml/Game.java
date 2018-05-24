e8f441861e0e1c58278facbcbbc721e6baa219b7
buike <buike@192.168.171.2>
2018-05-24 22:58:53 +0700

    buoc 1

--- xml/src/xml/Game.java
+++ xml/src/xml/Game.java
@@ -1,5 +1,37 @@
 package xml;
 
+import java.util.List;
+
 public class Game {
+public Maps map;
+public List<Player> lPlayer;
+public Game(Maps map, List<Player> lPlayer) {
+	super();
+	this.map = map;
+	this.lPlayer = lPlayer;
+}
+public Maps getMap() {
+	return map;
+}
+public void setMap(Maps map) {
+	this.map = map;
+}
+public List<Player> getlPlayer() {
+	return lPlayer;
+}
+public void setlPlayer(List<Player> lPlayer) {
+	this.lPlayer = lPlayer;
+}
+@Override
+public String toString() {
+	StringBuilder builder = new StringBuilder();
+	builder.append("Game [map=");
+	builder.append(map);
+	builder.append(", lPlayer=");
+	builder.append(lPlayer);
+	builder.append("]");
+	return builder.toString();
+}
+
 
 }
